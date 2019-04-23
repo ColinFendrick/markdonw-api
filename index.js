2566d7be34eccfe6e82a9d89451d5a24cb1b40b6
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-23 10:13:02 -0400

    Add updated index with post calls

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000);
+app.post('/login', function (req, res) {
+  res.send("Authenticated");
+},
+);
+
+app.post("/convert", function (req, res, next) {
+  console.log(req.body);
+  if (typeof req.body.content == 'undefined' || req.body.content == null) {
+    res.json(["error", "No data found"]);
+  } else {
+    res.json(["markdown", req.body.content]);
+  }
+});
+
+app.listen(3000, function () {
+  console.log("Server running on port 3000");
+});
