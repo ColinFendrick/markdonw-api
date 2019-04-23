d91cb98dcb5d26b8217a10ec1a3ead4469ccefda
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-23 10:14:12 -0400

    Add showdown

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const showdown = require('showdown');
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+converter = new showdown.Converter();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -15,14 +18,16 @@ app.post('/login', function (req, res) {
 );
 
 app.post("/convert", function (req, res, next) {
-  console.log(req.body);
   if (typeof req.body.content == 'undefined' || req.body.content == null) {
     res.json(["error", "No data found"]);
   } else {
-    res.json(["markdown", req.body.content]);
+    text = req.body.content;
+    html = converter.makeHtml(text);
+    res.json(["markdown", html]);
   }
 });
 
 app.listen(3000, function () {
   console.log("Server running on port 3000");
 });
+
